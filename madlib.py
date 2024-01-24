b19f632d4421198e31c510024f3863d417f6c31d
rueph <ndetoruth603@gmail.com>
2024-01-24 11:07:17 +0300

    first commit

---
+++ madlib.py
@@ -0,0 +1,17 @@
+adjective = input("Enter Adjective : ")
+color = input("Enter Color Name : ")
+noun1 = input("Enter noun  : ")
+noun1 = input("Enter noun  : ")
+noun1 = input("Enter noun  : ")
+noun1 = input("Enter noun  : ")
+noun2 = input("Enter noun  : ")
+noun3 = input("Enter noun  : ")
+noun4 = input("Enter noun  : ")
+noun5 = input("Enter noun  : ")
+proper_noun = input("Enter proper noun: ")
+adjective2 = input("Enter adjective: ")
+verb = input("Enter verb")
+
+
+
+print(f"Last night I dreamt I was a {adjective} , butterfly with {color} clothes that looked like {noun1}. I flew to {noun2} with my best friend and {proper_noun} who was a {adjective2} {noun3}. We ate some {noun4} when we got there and then decided to {verb} and the dream ended when I said let's {verb}.")
